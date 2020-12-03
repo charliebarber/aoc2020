f507534fcac62e7225f2b62feb919b0eef7456b8
Charlie Barber <charlieb@protonmail.com>
2020-12-03 19:47:06 +0000

    Day 3 part 1

--- 3.js
+++ 3.js
@@ -5,5 +5,23 @@ fs.readFile('input3.txt', 'utf8', (err, data) => {
 })
 
 const main = (data) => {
-    console.log(data)
+    const lines = data.split('\n')
+    lines.splice(323,1)
+    const map = lines.map(line => line.split(''))
+
+    let posX = 0
+    let posY = 0
+    let treeCount = 0
+
+    for (posY = 1; posY <= lines.length - 1; posY += 1) {
+        posX += 3
+        if (posX > 30) {
+            posX = (posX - 1) % 30
+        }
+        if (map[posY][posX].includes('#')) {
+            treeCount += 1
+        }
+    }
+
+    console.log(treeCount)
 }
